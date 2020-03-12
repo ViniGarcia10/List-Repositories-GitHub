a3f5bda7a3a7417429aa5643e48a4d3c1066caac
ViniGarcia10 <viniciusgarcia49@gmail.com>
2020-03-12 07:31:54 -0400

    config react-navigation v5

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,16 +1,11 @@
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
 
 import './config/ReactotronConfig';
 
-console.tron.warn('hello world!');
+import Routes from './routes';
 
 export default class App extends Component {
   render() {
-    return (
-      <View>
-        <Text> React Native </Text>
-      </View>
-    );
+    return <Routes />;
   }
 }
