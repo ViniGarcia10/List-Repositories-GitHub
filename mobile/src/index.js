ad8f234b846c52458ebef6dfe7cbad2550ff97ed
ViniGarcia10 <viniciusgarcia49@gmail.com>
2020-03-12 05:15:41 -0400

    config reactotron

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,6 +1,10 @@
 import React, {Component} from 'react';
 import {Text, View} from 'react-native';
 
+import './config/ReactotronConfig';
+
+console.tron.warn('hello world!');
+
 export default class App extends Component {
   render() {
     return (
