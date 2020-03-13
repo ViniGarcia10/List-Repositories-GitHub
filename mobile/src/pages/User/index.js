1b830c2e5bbedd179b962c44e7b3c9c65e60bf6a
ViniGarcia10 <viniciusgarcia49@gmail.com>
2020-03-13 08:03:40 -0400

    stylind page user

--- mobile/src/pages/User/index.js
+++ mobile/src/pages/User/index.js
@@ -1,10 +1,78 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import api from '../../services/api';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Name,
+  Bio,
+  Avatar,
+  Header,
+  Starred,
+  Stars,
+  OwnerAvatar,
+  Info,
+  Title,
+  Author,
+} from './styles';
 
-export default function User({route}) {
-  const user = route.params.user;
+export default class User extends Component {
+  static propTypes = {
+    navigation: PropTypes.shape({
+      getParam: PropTypes.func,
+    }).isRequired,
+  };
 
-  return <Text>Usuário: {user.name}</Text>;
+  state = {
+    user: {},
+    stars: [],
+  };
+
+  async handleData() {
+    const {navigation, route} = this.props;
+
+    const user = await route.params.user;
+
+    this.setState({user});
+
+    await navigation.setOptions({title: user.name});
+  }
+
+  async componentDidMount() {
+    await this.handleData();
+
+    console.tron.log(this.state.user.login);
+
+    const response = await api.get(`/users/${this.state.user.login}/starred`);
+
+    this.setState({stars: response.data});
+  }
+
+  render() {
+    const {stars, user} = this.state;
+
+    return (
+      <Container>
+        <Header>
+          <Avatar source={{uri: user.avatar}} />
+          <Name>{user.name}</Name>
+          <Bio>{user.bio}</Bio>
+        </Header>
+
+        <Stars
+          data={stars}
+          keyExtractor={star => String(star.id)}
+          renderItem={({item}) => (
+            <Starred>
+              <OwnerAvatar source={{uri: item.owner.avatar_url}} />
+              <Info>
+                <Title>{item.name}</Title>
+                <Author>{item.owner.login}</Author>
+              </Info>
+            </Starred>
+          )}
+        />
+      </Container>
+    );
+  }
 }
